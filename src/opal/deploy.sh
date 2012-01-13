5ec8e772eda61aa56436869c299ff3bc0ab6237b
Luca Clementi <luca.clementi@gmail.com>
2012-01-13 03:47:45 +0000

    First round of fixes

--- src/opal/deploy.sh
+++ src/opal/deploy.sh
@@ -13,38 +13,3 @@ ant -f $OPALXML deploy -DserviceName=date  -DappConfig=$OPALCONFIG/date_config.x
 
 # deploy existing services  
 
-pdb2pqr_bin=/opt/pdb2pqr
-if test -e $pdb2pqr_bin; then
-    ant -f $OPALXML deploy -DserviceName=PDB2PQR -DappConfig=$OPALCONFIG/pdb2pqr_config.xml
-fi
-
-
-apbs_bin=/opt/apbs/bin/apbs
-if test -e $apbs_bin; then
-    ant -f $OPALXML deploy -DserviceName=APBS -DappConfig=$OPALCONFIG/apbs_config.xml
-fi
-
-mast_bin=/opt/meme/bin/mast
-if test -e $mast_bin; then
-    ant -f $OPALXML deploy -DserviceName=MAST -DappConfig=$OPALCONFIG/mast_config.xml
-fi  
-
-meme_bin=/opt/meme/bin/meme
-if test -e $meme_bin; then
-    ant -f $OPALXML deploy -DserviceName=MEME -DappConfig=$OPALCONFIG/meme_config.xml
-fi  
-
-autogrid=/opt/bio/autodocksuite/bin/autogrid4
-if test -e $autogrid; then
-    ant -f $OPALXML deploy -DserviceName=AutoGrid -DappConfig=$OPALCONFIG/autogrid_config.xml
-fi  
-
-autodock=/opt/bio/autodocksuite/bin/autodock4
-if test -e $autodock; then
-    ant -f $OPALXML deploy -DserviceName=AutoDock -DappConfig=$OPALCONFIG/autodock_config.xml
-fi  
-
-babel=/opt/bio/bin/babel
-if test -e $babel; then
-    ant -f $OPALXML deploy -DserviceName=babel -DappConfig=$OPALCONFIG/babel_config.xml
-fi  
