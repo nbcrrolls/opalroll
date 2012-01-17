3e1d34adb73cfd60f29d85575b949fc65b596939
Luca Clementi <luca.clementi@gmail.com>
2012-01-17 03:38:46 +0000

    an exit in a post install script!! More fixes but it still doesn't work :-(

--- src/opal/deploy.sh
+++ src/opal/deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+ANT=/opt/rocks/bin/ant
 OPALXML=/opt/opal/build.xml
 OPALCONFIG=/opt/opal/configs
 
 # deploy test applications
-ant -f $OPALXML deploy -DserviceName=cat   -DappConfig=$OPALCONFIG/cat_config.xml
-ant -f $OPALXML deploy -DserviceName=date  -DappConfig=$OPALCONFIG/date_config.xml
+$ANT -f $OPALXML deploy -DserviceName=cat   -DappConfig=$OPALCONFIG/cat_config.xml
+$ANT -f $OPALXML deploy -DserviceName=date  -DappConfig=$OPALCONFIG/date_config.xml
 
 # deploy meta services  
 # ant -f $OPALXML deploy -DserviceName=APBSParallelMeta -DappConfig=$OPALCONFIG/apbs_parallel_meta_config.xml
